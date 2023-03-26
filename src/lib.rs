d7c7acb335a03634f19ffa9c705b513bb4d516de
Xetera <contact@xetera.dev>
2023-03-26 05:38:06 +0300

    re-exporting cookies

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ mod request;
 
 pub use clokwerk::{AsyncJob, AsyncScheduler};
 pub use maera::*;
+pub use ratmom::cookies;
 pub use ratmom::http::{Method, Request, Response, StatusCode};
 pub use ratmom::AsyncBody;
 pub use ratmom::AsyncReadResponseExt;
